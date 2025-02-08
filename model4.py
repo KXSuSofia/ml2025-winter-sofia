f37c297d1dde50b2c1f35dc9a8169741eb55d4b8
KXSuSofia <sukexuan@gmail.com>
2025-02-08 12:46:24 -0800

    Update model4.py

--- model4.py
+++ model4.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 N = int(input("Enter a positive integer N: "))  
 numbers = [int(input(f"Enter number {i+1}: ")) for i in range(N)]  
 X = int(input("Enter an integer X: "))  
 
 print(numbers.index(X) + 1 if X in numbers else -1)
 
-
-# In[ ]:
 
 
 
