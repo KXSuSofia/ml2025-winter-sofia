e2c948982da365b6444a4e1dca7a5a5b857bb6eb
KXSuSofia <sukexuan@gmail.com>
2025-02-16 04:06:55 -0800

    Create module5_mod.py

---
+++ module5_mod.py
@@ -0,0 +1,12 @@
+class NumberProcessor:
+    def __init__(self):
+        self.numbers = []
+
+    def add_number(self, number):
+        self.numbers.append(number)
+
+    def find_number(self, target):
+        if target in self.numbers:
+            return self.numbers.index(target) + 1  
+        else:
+            return -1
